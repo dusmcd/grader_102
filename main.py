1ced69433ef7919436370081574905239564ad03
dusmcd <dustinmcdowell1989@gmail.com>
2026-02-14 20:58:09 -0500

    handling multiple submissions; need to test

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 import shutil
 import subprocess
 from queue import Queue
+from datetime import datetime
 
 SEP = os.sep
 BROWSER = "firefox"
@@ -16,10 +17,15 @@ def get_data_from_txt_file(file):
         if "Filename" in words[i]:
             break
         filename.append(words[i]) 
+    
+    for i in range(len(words)):
+        if "Submitted" in words[i]:
+            date_str = str.join(" ", words[i + 1:i + 7])
+            break
 
     filename.reverse()
 
-    return (f"{words[1]} {words[2]}", " ".join(filename))
+    return (f"{words[1]} {words[2]}", " ".join(filename), date_str)
 
 def find_html_files(path="current", num_files=1):
     # perform breadth-first search
@@ -43,14 +49,22 @@ def find_html_files(path="current", num_files=1):
 
     return files
 
+def get_latest_submission(subs):
+    latest_entry =  max(subs, lambda sub : sub[1])
+    return latest_entry[0]
+    
 
 def process_submissions(subs, num_files):
     count = 0
     for key, val in subs.items():
         print(f"Currently grading: {key}")
-        if not val:
-            print("There are multiple submissions for this student. Process manually\n")
-        elif val[-4:].lower() == "html":
+        if len(val) > 1:
+            print("There are multiple submissions for this student. Choosing latest submission...\n")
+            val = get_latest_submission(val)
+        else:
+            # just grab the filename of the first entry (a list of tuples)
+            val = val[0][0]
+        if val[-4:].lower() == "html":
             print(f"Opening {val} in browser...")
             subprocess.run([BROWSER, f"{os.getcwd()}{SEP}submissions{SEP}{val}"])
         elif val[-3:].lower() == "zip":
@@ -97,11 +111,13 @@ def main():
 
     for file in os.listdir("submissions"):
         if file[-3:] == "txt":
-            data = get_data_from_txt_file(f"submissions{SEP}{file}")
-            if data[0] in num_submissions:
-                num_submissions[data[0]] = False
+            (name, filename, date_str) = get_data_from_txt_file(f"submissions{SEP}{file}")
+            file_datetime = datetime.strptime(date_str, "%A, %B %d, %Y %I:%M:%S %p") 
+            print(f"file_datetime: {file_datetime}")
+            if name in num_submissions:
+                num_submissions[name].append((filename, file_datetime))
             else:
-                num_submissions[data[0]] = data[1]
+                num_submissions[name] = [(filename, file_datetime)]
 
     process_submissions(num_submissions, num_files)
 
